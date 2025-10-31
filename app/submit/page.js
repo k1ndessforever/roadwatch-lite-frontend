5279ce474a96d587dd27ecb40696577226107eaa
Yash Pandit <yash.pandit.dev01@gmail.com>
2025-10-31 18:47:16 +0530

    Update page.js

--- app/submit/page.js
+++ app/submit/page.js
@@ -1,6 +1,6 @@
 // frontend/app/submit/page.js
 'use client';
-import SubmitForm from '@/components/SubmitForm';
+import SubmitForm from '../components/SubmitForm';
 
 export default function SubmitPage() {
   return (
