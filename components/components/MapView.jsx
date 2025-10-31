d4a3a4a3b837dc18e7560e1ceadd0a0a5d38b372
k1ndnessforever <yash.pandit.dev01@gmail.com>
2025-10-31 18:39:08 +0530

    Fix: Remove nested components folder

--- components/components/MapView.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function MapView() {
-  return (
-    <div className='w-full h-96 bg-gray-200 rounded-lg flex items-center justify-center'>
-      <p className='text-gray-500'>Map view coming soon...</p>
-    </div>
-  );
-}
